00a7a607452ac4672dc489b39ea65fa39ca7f7c2
ivanrauda <ivan.dev4286@gmail.com>
2019-07-27 18:15:24 +0700

    Display message in client

--- resolvers/message.js
+++ resolvers/message.js
@@ -1,19 +1,36 @@
+import { requireAuth } from "../permissions";
+
 export default {
+  Message: {
+    user: async ({ userId }, args, { models }) =>
+      await models.User.findOne({ where: { id: userId } }, { raw: true })
+  },
   Query: {
-    messages: async (parent, args, { models, user }) => []
+    messages: requireAuth.createResolver(
+      async (parent, { channelId }, { models }) => {
+        const messages = await models.Message.findAll(
+          { order: [["createdAt", "ASC"]], where: { channelId } },
+          { raw: true }
+        );
+        console.log(messages);
+        return messages;
+      }
+    )
   },
   Mutation: {
-    createMessage: async (parent, args, { models, user }) => {
-      try {
-        await models.Message.create({
-          ...args,
-          userId: user.id
-        });
-        return true;
-      } catch (err) {
-        console.error(err);
-        return false;
+    createMessage: requireAuth.createResolver(
+      async (parent, args, { models, user }) => {
+        try {
+          await models.Message.create({
+            ...args,
+            userId: user.id
+          });
+          return true;
+        } catch (err) {
+          console.error(err);
+          return false;
+        }
       }
-    }
+    )
   }
 };
