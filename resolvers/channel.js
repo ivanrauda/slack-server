fe7299e3e08282e28666b63a8cfefeb1babeba4b
ivanrauda <ivan.dev4286@gmail.com>
2019-07-27 15:26:44 +0700

    display invite button and create channel for only owner: change owner in schema into Int type

--- resolvers/channel.js
+++ resolvers/channel.js
@@ -1,21 +1,39 @@
 import formatErrors from "../formatErrors";
+import { requireAuth } from "../permissions";
 
 export default {
   Mutation: {
-    createChannel: async (parent, args, { models }) => {
-      try {
-        const channel = await models.Channel.create(args);
-        return {
-          ok: true,
-          channel
-        };
-      } catch (err) {
-        console.log(err);
-        return {
-          ok: false,
-          errors: formatErrors(err, models)
-        };
+    createChannel: requireAuth.createResolver(
+      async (parent, args, { models, user }) => {
+        try {
+          const team = await models.Team.findOne(
+            { where: { id: args.teamId } },
+            { raw: true }
+          );
+          if (team.owner !== user.id) {
+            return {
+              ok: false,
+              errors: [
+                {
+                  path: "name",
+                  message: "Only the owner of the team can create channels!"
+                }
+              ]
+            };
+          }
+          const channel = await models.Channel.create(args);
+          return {
+            ok: true,
+            channel
+          };
+        } catch (err) {
+          console.log(err);
+          return {
+            ok: false,
+            errors: formatErrors(err, models)
+          };
+        }
       }
-    }
+    )
   }
 };
